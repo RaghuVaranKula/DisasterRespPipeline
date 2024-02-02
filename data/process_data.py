01315e87c65bbfd6907e44ef3ba38ab6ff993a28
Raghu Kula <raghuvarankula@gmail.com>
2024-02-02 19:13:59 +0530

    Updated with required changes

--- data/process_data.py
+++ data/process_data.py
@@ -1,38 +1,53 @@
 import sys
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,inspect
 
 def load_data(messages_filepath, categories_filepath):
-    '''
-    input:
-        messages_filepath: Load messages.csv into a dataframe .
-        categories_filepath: Load categories.csv into a dataframe.
-    output:
-        df: Merged messages and categories datasets using the common id
-    '''
+    """
+    Load and merge messages and categories datasets from specified file paths.
+
+    Parameters:
+    - messages_filepath (str): File path for the messages CSV file.
+    - categories_filepath (str): File path for the categories CSV file.
+
+    Returns:
+    - df (DataFrame): A pandas DataFrame containing merged content of messages and categories datasets,
+                      merged on the common 'id' column.
+
+    The function reads the messages and categories datasets from their respective file paths,
+    then merges them into a single DataFrame on the 'id' column which is common to both datasets.
+    """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     df = pd.merge(messages, categories, left_on='id', right_on='id', how='outer')
     return df
 
 def clean_data(df):
-    '''
-    input:
-        df: Merged raw dataset without cleaning.
-    output:
-        df: Dataset after cleaning.
-    '''
+    """
+    Cleans the merged DataFrame by splitting categories into separate columns, 
+    converting data types, and removing duplicates.
+
+    Parameters:
+    - df (DataFrame): The merged DataFrame containing messages and categories data.
+
+    Returns:
+    - df (DataFrame): The cleaned DataFrame with categories split into separate columns,
+                      data types standardized, and duplicates removed.
+
+    This function assumes that the 'categories' column in the DataFrame contains 
+    multiple categories separated by semicolons, which it splits into separate 
+    boolean columns. It also ensures there are no duplicate entries based on message ids.
+    """
     #Split the values in the categories column on the ';' 
     #character so that each value becomes a separate column.
     categories = df.categories.str.split(';', expand = True)
     row = categories.loc[0] # use this row to extract a list of new column names for categories
     category_colnames = row.apply(lambda x: x[:-2]).values.tolist()
     categories.columns = category_colnames
-    # categories.related.loc[categories.related == 'related-2'] = 'related-1'
+    categories.related.loc[categories.related == 'related-2'] = 'related-1' # replace all 2 with 1
     for column in categories:
         # set each value to be the last character of the string
         categories[column] = categories[column].astype(str).str[-1]
-        # convert column from string to numeric
         categories[column] = pd.to_numeric(categories[column])
     # drop the original categories column from `df`
     df.drop('categories', axis = 1, inplace = True)
@@ -43,15 +58,50 @@ def clean_data(df):
         df.drop_duplicates(subset = 'id', inplace = True)
     return df
 
-def save_data(df, database_filepath):
-    engine = create_engine('sqlite:///' + database_filepath)
-    df.to_sql('Message', engine, index=False)
-    if(engine.has_table('Message')):
+def save_data(df, database_filename):
+    """
+    Saves the cleaned DataFrame to a SQLite database.
+
+    Parameters:
+    - df (DataFrame): The cleaned DataFrame to be saved.
+    - database_filename (str): The file path of the SQLite database where the DataFrame will be saved.
+
+    The function creates a SQLite database (if it doesn't exist) and saves the DataFrame
+    into a table named 'CleanedData'. It overwrites any existing table with the same name.
+    """
+    engine = create_engine(f'sqlite:///' + database_filename)
+    df.to_sql('Message', engine, index=False, if_exists='replace')
+    # Create an inspector object
+    inspector = inspect(engine)
+
+    if(inspector.has_table('Message')):
         print("DB created successfully")
     else:
         print("Failed to create DB")
 
 def main():
+    """
+    The main function of a data processing script that loads, cleans, and saves disaster response data.
+
+    This function orchestrates the data processing pipeline by performing the following steps:
+    1. Checks if the correct number of command-line arguments (paths) are provided.
+    2. Loads the disaster messages and categories data from specified file paths.
+    3. Cleans the combined data.
+    4. Saves the cleaned data to a SQLite database at the specified file path.
+
+    The function requires exactly three command-line arguments to run successfully:
+    - The first argument is the file path to the disaster messages data file.
+    - The second argument is the file path to the disaster categories data file.
+    - The third argument is the file path to the SQLite database where the cleaned data will be stored.
+
+    If the correct number of arguments is not provided, the function prints an error message with an example command.
+
+    Parameters:
+    - None
+
+    Returns:
+    - None
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,27 +1,143 @@
+# import libraries
+import warnings
+warnings.filterwarnings("ignore")
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 import sys
+import re
+import nltk 
+nltk.download(['punkt','wordnet','averaged_perceptron_tagger'])
+
+import pandas as pd
+import pickle # For serializing and deserializing Python object structures.
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.metrics import classification_report,accuracy_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.svm import LinearSVC # SVM model for classification tasks.
+from sklearn.multiclass import OneVsRestClassifier # Strategy for multi-class classification.
 
 
 def load_data(database_filepath):
-    pass
+    """
+    Loads data from a SQLite database and returns feature and target datasets.
 
+    Parameters:
+    - database_filepath (str): The filesystem path to the SQLite database file.
+
+    Returns:
+    - X (pandas.DataFrame): The features dataset extracted from the database.
+    - y (pandas.DataFrame): The target variable dataset extracted from the database.
+
+    """
+
+    # load data from database
+    engine = create_engine('sqlite:///'+ database_filepath)
+    df = pd.read_sql_table("Message", engine)
+    X = df['message']
+    y = df.iloc[:,4:]
+    y['related'].replace(2, 1, inplace=True)
+    category_names = list(df.columns[4:])
+    return X, y, category_names
+    
 
 def tokenize(text):
-    pass
+    """
+    Tokenizes text into words.
+
+    Parameters:
+    - text (str): The text to be tokenized.
+
+    Returns:
+    - list[str]: A list of words tokenized and lemmatized from the text.
+    """
+     
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower()) #Normalize text
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """
+    Constructs and returns a machine learning model pipeline.
+
+    Returns:
+    - model (Pipeline): A scikit-learn Pipeline object that encapsulates the
+      preprocessing steps and the classifier with GridSearch.
+
+    """
+
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(OneVsRestClassifier(LinearSVC(dual=False, random_state = 0))))
+    ])
+    parameters = {
+                'tfidf__smooth_idf':[True, False],
+                'clf__estimator__estimator__C': [1, 2, 5]
+             }
+    cv = GridSearchCV(pipeline, param_grid=parameters, scoring='precision_samples', cv = 5,n_jobs=-1,verbose=2)
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+    Evaluates and prints the performance of the machine learning model on a test dataset.
+
+    Parameters:
+    - model (Pipeline): The machine learning model to evaluate.
+    - X_test (pandas.DataFrame): The features of the test dataset.
+    - y_test (pandas.DataFrame or pandas.Series): The true labels of the test dataset.
+    """
+
+    Y_pred = model.predict(X_test)
+    print("Accuracy")
+    print((Y_pred == Y_test).mean())
+    for i, col in enumerate(category_names):
+        print('{} category metrics: '.format(col))
+        print(classification_report(Y_test.iloc[:,i], Y_pred[:,i]))
+        print(accuracy_score(Y_test.iloc[:,i], Y_pred[:,i]))
+    print('---------------------------------')
 
 
 def save_model(model, model_filepath):
-    pass
+    """
+    Serializes and saves the machine learning model to a specified file path.
+
+    Parameters:
+    - model (Pipeline): The machine learning model to save.
+    - model_filepath (str): The path where the model should be saved, including the filename.
+    """
+
+    pickle.dump(model, open(model_filepath, 'wb')) #dumping pickle file
 
 
 def main():
+    """
+    Main function to orchestrate the data loading, model training, evaluation, and saving.
+    It specifically handles the following tasks:
+    - Loading the data from a predefined source.
+    - Building a machine learning model with predefined parameters.
+    - Training the model on the loaded data.
+    - Evaluating the model's performance on a test dataset.
+    - Saving the trained model to disk for future use.
+
+    Raises:
+    - Exception: Describes any exceptions that might be raised during the execution
+      of the function, such as issues with data loading, model training failures,
+      or errors during model saving.
+    """
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
